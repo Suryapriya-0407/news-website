82a90870d52d32c52e0a2f39f527d3d4f9f9559d
Suryapriya Prahladan <121708752+Suryapriya-0407@users.noreply.github.com>
2023-03-03 19:47:29 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+AOS.init();
+
+
+
+ function openFunction(){
+  document.getElementById("menu").style.width="300px";
+  document.getElementById("mainbox").style.marginLeft="0px";
+  
+ }
+function closeFunction(){
+ document.getElementById("menu").style.width="0px";
+ document.getElementById("mainbox").style.marginLeft="0px";
+ 
+}
